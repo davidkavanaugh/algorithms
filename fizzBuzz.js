95480e3cd4b633f05266eaef5fae3134cc900d91
David <davidkavanaugh@outlook.com>
2024-01-08 19:52:32 -0800

    starter algos

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// iterate over a given list of numbers
+// if the number is divisible by 3 and 5, print FizzBuzz
+// else if the number is divisible by 3, print Fizz
+// else if the number is divisible by 5, print buzz
+// else print the number
+
+const listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+function fizzBuzz(list) {
+
+};
+
+fizzBuzz(listOfNumbers);
---
+++ sum2.js
@@ -0,0 +1,7 @@
+// print the sum of two given numbers
+
+function sum2(num1, num2) {
+
+};
+
+sum2(1,2);
